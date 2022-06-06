82beb7b3e53383784b2350282d019b4a12f339f6
SilvanGrosskreutz <silvan.grosskreutz@htp-tel.de>
2022-06-06 09:24:28 +0200

    Components + stuff added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    RegisterComponent,
+    LoginComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
