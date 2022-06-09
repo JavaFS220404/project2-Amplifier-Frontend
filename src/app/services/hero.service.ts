64552bce8a95f283b0964d302da144f752901821
Tanuja Gowrineni <103101727+TanujaGowrineni@users.noreply.github.com>
2022-06-09 23:36:41 +0200

    Update hero.service.ts

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -23,7 +23,6 @@ export class HeroService {
     return this.http.post(this.url,hero,{withCredentials:true}) as Observable<Hero>
   }
 
-<<<<<<< HEAD
   getHeroById(id:number):Observable<any>{
       // fetch from api
       return this.http.get('https://akabab.github.io/superhero-api/api/id/'+id+'.json') as Observable<any>
@@ -35,20 +34,12 @@ export class HeroService {
     return this.http.get('https://www.superheroapi.com/api.php/100277516057979/search/' + name) as Observable<any>
   }
 
-  
-=======
   updateHero(hero:Hero):Observable<Hero>{
     console.log(hero);
     return this.http.put(this.url,hero,{withCredentials:true}) as Observable<Hero>
   }
 
-// To verify, eventually no hero and any instead of
-getHeroById(id:number):Observable<any>{
-  return this.http.get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/' + id +".json") as Observable<any>
-}
-
-logout(){
-  this.userService.activeUser = null;
-}
->>>>>>> 91cb44baa345755776cf06736380ffc951334b67
+  logout(){
+   this.userService.activeUser = null;
+  }
 }
